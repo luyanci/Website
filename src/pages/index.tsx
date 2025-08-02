0908863851fc646dba58bcba63475eda7f1028a2
Saksham <198061763+TypeFlu@users.noreply.github.com>
2025-08-02 18:30:23 +0530

    use new logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <div className={styles.heroLogo}>
-          <img src="/img/logo.png" alt="SukiSU Ultra Logo" className={styles.logoImage} />
+          <img src="/img/logo.svg" alt="SukiSU Ultra Logo" className={styles.logoImage} />
         </div>
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
