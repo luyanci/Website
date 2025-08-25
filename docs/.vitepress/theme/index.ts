77e7b518cea8488871a324fa37ca8a219c2705fd
YC酱luyancib <luyancib@qq.com>
2025-08-25 17:57:14 +0800

    feat:add git changelog plugin

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,13 @@
 // .vitepress/theme/index.ts
 import DefaultTheme from 'vitepress/theme'
+import { NolebaseGitChangelogPlugin } from '@nolebase/vitepress-plugin-git-changelog/client'
 import 'virtual:group-icons.css'
 import { h } from 'vue'
 import './style/style.css'
 import Confetti from './components/Confetti.vue'
 import ArticleShare from './components/ArticleShare.vue'
 import backtotop from './components/backtotop.vue'
+import '@nolebase/vitepress-plugin-git-changelog/client/style.css'
 
 export default {
   extends: DefaultTheme,
@@ -16,6 +18,7 @@ export default {
     })
   },
   enhanceApp({ app }) {
+    app.use(NolebaseGitChangelogPlugin);
     app.component('Confetti', Confetti)
   },
 }
