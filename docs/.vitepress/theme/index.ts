c8ae1a42fe323a7186317e587a9b62d32c3262d7
YC酱luyancib <luyancib@qq.com>
2025-08-25 17:57:14 +0800

    feat:add git changelog plugin

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,24 +1,26 @@
 // .vitepress/theme/index.ts
 import DefaultTheme from 'vitepress/theme'
+import { NolebaseGitChangelogPlugin } from '@nolebase/vitepress-plugin-git-changelog/client'
 import 'virtual:group-icons.css'
 import {h} from 'vue'
 import './style/style.css'
 import Confetti from './components/Confetti.vue'
 import ArticleShare from './components/ArticleShare.vue'
-import backtotop from "./components/backtotop.vue";
-
+import backtotop from './components/backtotop.vue'
+import '@nolebase/vitepress-plugin-git-changelog/client/style.css'
 
 export default {
-    extends: DefaultTheme,
-    Layout: () => {
-        return h(DefaultTheme.Layout, null, {
-            "aside-outline-before": () => h(ArticleShare),
-            'doc-footer-before': () => h(backtotop),
-        })
-    },
-    enhanceApp({app}) {
-        app.component("Confetti", Confetti);
-    }
+  extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      'aside-outline-before': () => h(ArticleShare),
+      'doc-footer-before': () => h(backtotop),
+    })
+  },
+  enhanceApp({ app }) {
+    app.use(NolebaseGitChangelogPlugin);
+    app.component('Confetti', Confetti)
+  },
 }
 
 
